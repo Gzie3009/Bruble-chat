99451b16842c797cd31c041becb6ba9d1c80f533
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-09-19 00:12:51 +0530

    created settings drawer

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -22,11 +22,22 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      onClick={() => onclick}
+      onClick={onCLick}
       disabled={disabled}
       type={type}
       className={clsx(
-        "flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+        `
+        flex 
+        justify-center 
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `,
         disabled && "opacity-50 cursor-default",
         fullwidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
